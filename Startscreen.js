8e81a6f17cc41d26fe3353a6aaea9f3026a2e3d9
julienB700 <118830153+julienB700@users.noreply.github.com>
2023-05-11 15:12:26 +0200

    MEP

--- Startscreen.js
+++ Startscreen.js
@@ -8,9 +8,9 @@ class Menu extends Phaser.Scene {
         this.load.image('3P', 'assets/MenuScreenthirdPlan.png');
         this.load.image('4P', 'assets/MenuScreenFond.png');
         this.load.image('titre', 'assets/titre.png');
-        this.load.image('Button1', 'assets/NewGame.png');
-        this.load.image('Button2', 'assets/Continue.png');
-        this.load.image('Button3', 'assets/Options.png');
+        this.load.image('Button1', 'assets/NewGameButton.png');
+        this.load.image('Button2', 'assets/MenuScreenContinue.png');
+        this.load.image('Button3', 'assets/MenuScreenOptions.png');
 
     }
     create() {
@@ -23,17 +23,21 @@ class Menu extends Phaser.Scene {
         this.image = this.add.image
         this.image = this.add.image(449, 224, 'titre')
 
-        var button = this.add.sprite(449 ,224, 'Button1');
+        var button = this.add.sprite(449 ,270, 'Button1');
         button.setInteractive();
         button.on('pointerdown', () => {
             this.Startgame();
         });
-
-        this.image = this.add.image
-        this.image = this.add.image(449, 224, 'Button2')
-
-        this.image = this.add.image
-        this.image = this.add.image(449, 224, 'Button3')
+        var button = this.add.sprite(400 ,340, 'Button2');
+        button.setInteractive();
+        button.on('pointerdown', () => {
+            this.Startgame();
+        });
+        var button = this.add.sprite(380 ,400, 'Button3');
+        button.setInteractive();
+        button.on('pointerdown', () => {
+            this.Startgame();
+        });
 
 
         this.image = this.add.image
